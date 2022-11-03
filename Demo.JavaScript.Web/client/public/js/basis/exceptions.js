336938afaf7882984fae4f8c2fdd0bbd2728b00f
DavidUrting <david.urting@gmail.com>
2022-11-03 20:50:32 +0100

    finally block + unreachable code (console.log)

--- Demo.JavaScript.Web/client/public/js/basis/exceptions.js
+++ Demo.JavaScript.Web/client/public/js/basis/exceptions.js
@@ -1,9 +1,16 @@
 ﻿console.log("*** exceptions.js ***");
 
 try {
-    throw "Er is een onverwachte fout opgetreden, contacteer de helpdesk."
+    throw "Er is een onverwachte fout opgetreden, contacteer de helpdesk.";
+    
+    console.log("Dit zal nooit gelogd worden...");
 }
 catch (e) {
         // Normaal toon je dat aan de gebruiker (liefst niet met alert!) en log je dat in de databank.
     console.error(e);
 }
+finally {
+    console.log("Een finally block wordt altijd uitgevoerd (ongeacht of er een exception is opgetreden of niet).");
+}
+
+console.log("Dit zal wel gelogd worden...");
