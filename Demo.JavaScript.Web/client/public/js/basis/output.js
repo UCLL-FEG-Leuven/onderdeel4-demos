9f8dd644c9bb95a49f5c8f6be09d391c305e1388
DavidUrting <david.urting@gmail.com>
2022-11-03 20:03:46 +0100

    output.js nog wat uitgebreid met voorbeelden.

--- Demo.JavaScript.Web/client/public/js/basis/output.js
+++ Demo.JavaScript.Web/client/public/js/basis/output.js
@@ -23,4 +23,11 @@ document.getElementById("basics-output-dom").innerText = "Deze tekst werd vanuit
 document.getElementById("basics-output-dom2").innerHTML = "Je kan ook een HTML string genereren en invoegen.<ul><li>Zo kan je dynamisch HTML genereren</li><li>Later zullen we ook zien hoe we dat via de DOM kunnen doen.</li></ul>";
 document.getElementById("basics-output-dom3").value = "Ook de value en styling kan je instellen.";
 document.getElementById("basics-output-dom3").className = "bg-warning"; // en ook de styling kan je aanpassen!
+document.getElementById("basics-output-dom4").style.display = "none";
 
+
+let alleH3sInSection = document.querySelectorAll("section h3");
+console.log("Aantal H3's in sections: " + alleH3sInSection.length);
+
+let eersteH3InSection = document.querySelector("section h3");
+console.log("Tekst van eerste H3 in section: " + eersteH3InSection.innerText);
