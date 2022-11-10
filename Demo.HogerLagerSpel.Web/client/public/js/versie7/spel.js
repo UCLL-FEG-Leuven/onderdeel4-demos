9092a5dd4670f95ae186a7c9edeaf0206b443cac
davurt <david.urting@gmail.com>
2022-11-11 00:40:24 +0100

    versie 7 (classes) en versie 8 (modules). De vorige versie 7 (webpack) is nu versie 9 geworden. Versie 9 bouwt verder op versie 8.

---
+++ Demo.HogerLagerSpel.Web/client/public/js/versie7/spel.js
@@ -0,0 +1,44 @@
+class Spel {
+    constructor(htmlElementToRenderIn) {
+        this._htmlElementToRenderIn = htmlElementToRenderIn;
+    }
+
+    startSpel() {
+        this._score = 0;
+        this._teStellenVragen = [];
+
+        let loadedVragen = VragenLoader.loadVragen();
+        while (loadedVragen.length > 0) {
+            let vraagIndex = Math.random() * loadedVragen.length; // [0...,1...[ -> [0..., loadedVragen.length[ 
+            vraagIndex = Math.floor(vraagIndex); // [0, loadedVragen.length[ -> [0, loadedVragen.length-1]
+            this._teStellenVragen.push(loadedVragen[vraagIndex]);
+            loadedVragen.splice(vraagIndex, 1);
+        }
+
+        this._huidigeVraag = this._teStellenVragen.pop();        
+    }
+
+    render() {        
+        this._htmlElementToRenderIn.innerHTML = 
+            `<section>
+                <p id="vraag">        
+                </p>
+            </section>
+            <section>
+                <p>Uw score is: ${this._score}</p>
+            </section>`;
+
+        if (this._huidigeVraag != null) {
+            this._huidigeVraag.render(document.getElementById("vraag"), (juist) => {
+                if (juist) this._score++;
+                
+                if (this._teStellenVragen.length > 0) {
+                    this._huidigeVraag = this._teStellenVragen.pop();
+                } else {
+                    this._huidigeVraag = null;
+                }            
+                this.render();
+            });    
+        }
+    }
+}
